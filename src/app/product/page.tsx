4dc364cd305af429890c0935daabf971a846475d
Vitor <vitordossantosdomingues@gmail.com>
2025-08-06 05:23:09 -0300

    chore: update shadcn theme

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,11 +1,9 @@
 import { Button } from '@/components/ui/button'
 
-import { cn } from '@/lib/utils'
-
 const Product = () => {
     return (
         <div>
-            <h1 className="mt-2 bg-red-500 px-10">Produto</h1>
+            <h1>Produto</h1>
             <Button>Comprar produto</Button>
         </div>
     )
